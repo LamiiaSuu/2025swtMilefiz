470bd17e2f64db5f4449eb504f595f9f429976be
rclaa001 <rclaa001@pc13-101.intern.mi.hs-rm.de>
2025-11-05 15:22:06 +0100

    initial commit

---
+++ src/main/java/de/hs_rm/de/milefiz/MilefizApplication.java
@@ -0,0 +1,13 @@
+package de.hs_rm.de.milefiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilefizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilefizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hs_rm/de/milefiz/MilefizApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hs_rm.de.milefiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilefizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
