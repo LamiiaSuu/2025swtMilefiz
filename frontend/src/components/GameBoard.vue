b6471442f67b2091e2e8fa2168a8ca5af47859d2
Leon Schäfer <leon.schaefer@student.hs-rm.de>
2025-11-10 14:25:13 +0100

    DONE: -Three & tresjs in main.ts eingebunden -GameBoard.vue angelegt --> beinhaltet eine Grüne Plane + blauen Canvas -App aufgeräumt und Gameboard hinzugefügt

---
+++ frontend/src/components/GameBoard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <TresCanvas window-size style="width: 100vw; height: 100vh;" clear-color="#87CEEB">
+    <TresPerspectiveCamera :position="[0, 8, 15]" />
+    <OrbitControls />
+    
+    <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
+      <TresPlaneGeometry :args="[20, 20]" />
+      <TresMeshBasicMaterial :color="0x7CFC00" />
+    </TresMesh>
+    
+    <TresAmbientLight :intensity="0.8" />
+  </TresCanvas>
+</template>
