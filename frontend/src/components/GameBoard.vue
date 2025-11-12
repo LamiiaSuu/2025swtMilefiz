eba3fdb4a7c677f945789de7c982e0f365b518c8
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-11-12 17:16:10 +0100

    Kommentare angepasst

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -34,12 +34,13 @@ onUnmounted(() => {
 </script>
 
 <template>
+  <!-- 3D-Canvas Element das den ganzen Bildschirm ausfüllt-->
   <TresCanvas
     window-size
     style="width: 100vw; height: 100vh"
     clear-color="#87CEEB"
   >
-    <!-- Orbit Kamera (Third Person) -->
+    <!-- Kamerapoistion und Kamerasteuerung via OrbitControls -->
     <TresPerspectiveCamera
       v-if="!useFirstPerson"
       ref="orbitCam"
@@ -56,16 +57,17 @@ onUnmounted(() => {
       :rotation="[0, 0, 0]"
     />
 
-    <!-- Boden -->
+    <!-- 3D-Objekt für den Spielfeld-Boden rotation dreht den boden, damit er horizontal und nicht
+     vertikal ist -->
     <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
       <TresPlaneGeometry :args="[20, 20]" />
       <TresMeshBasicMaterial :color="0x7cfc00" />
     </TresMesh>
 
-    <!-- Licht -->
+    <!-- Grundbeleuchtung der Szene (80% Intensität) -->
     <TresAmbientLight :intensity="0.8" />
 
-    <!-- Spielfigur -->
+    <!-- Game Character includiert (position - Position auf Plane), (bodyColor - Farbe der Figur), (eyeColor - Farbe der Augen) -->
     <GameCharacter
       ref="gameCharRef"
       :position="[0, 0, 0]"
