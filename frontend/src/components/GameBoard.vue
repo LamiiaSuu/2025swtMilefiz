6ae3052e854707522c27a9bf06ac32e1f4b427a3
Maximilian Ressel <Maximilian.Ressel@student.hs-rm.de>
2025-11-25 18:20:40 +0100

    jetzt frames auf 60 sorry

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -226,7 +226,7 @@ const onRotateCharacter = (yRotation: number) => {
 const checkHoverTile = () => {
 
   frameCounter++
-  if (frameCounter % 0 !== 0) {
+  if (frameCounter % 1 !== 0) {
     requestAnimationFrame(checkHoverTile)
     return
   }
