f2447e453b467c63a74fa34b1dcaa939e8f975b9
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-11-30 20:33:43 +0100

    CooldownServiceImpl realisiert

---
+++ src/main/java/de/hs_rm/de/milefiz/game/service/CooldownServiceImpl.java
@@ -0,0 +1,19 @@
+package de.hs_rm.de.milefiz.game.service;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Map;
+import java.util.UUID;
+
+
+@Service
+public class CooldownServiceImpl implements CooldownService {
+
+
+        public void tick() {
+
+        }
+}
