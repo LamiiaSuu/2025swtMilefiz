47d9b777ab1af2d85576d7b3d288186c2fb777e7
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-12-01 02:50:32 +0100

    tick() Methode implementiert - wird jede Sekunde ausgeführt. Ist schon getestet.

--- src/main/java/de/hs_rm/de/milefiz/game/service/CooldownServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/CooldownServiceImpl.java
@@ -1,13 +1,12 @@
 package de.hs_rm.de.milefiz.game.service;
 
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
-
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 
 @Service
@@ -31,7 +30,20 @@ public class CooldownServiceImpl implements CooldownService {
         return cooldowns.getOrDefault(playerId, 0);
     }
 
+    /**
+     * Wird automatisch jede Sekunde (1000ms) von Spring aufgerufen. In dieser Methode wird die Map 'cooldowns' durchiteriert und für jeden Spieler, sollte mindestens eine Sekunde übrig sein, eine Sekunde abgezogen.
+     */
+    @Override
+    @Scheduled(fixedRate = 1000) 
     public void tick() {
+        cooldowns.forEach((uuid, seconds) -> {
+            int newTime = seconds - 1;
 
+            if (newTime <= 0) {
+                cooldowns.remove(uuid); // Cooldown fertig -> spieler raus aus der Map
+            } else {
+                cooldowns.put(uuid, newTime);
+            }
+        });
     }
 }
