f17fdbd5a72e0f10a80cf2273d4377c32d71f72c
Leon Schäfer <leon.schaefer@student.hs-rm.de>
2025-12-05 12:46:52 +0100

    #153 GameServiceImpl: -Bezieht über InputStream das dummyBoard GameController: -Nutzt das über Inputstream gezogenen TestBoard BoardMapper: -Zieht sich über inputStream das dummyBoard

--- src/main/java/de/hs_rm/de/milefiz/game/controller/GameController.java
+++ src/main/java/de/hs_rm/de/milefiz/game/controller/GameController.java
@@ -1,32 +1,28 @@
 package de.hs_rm.de.milefiz.game.controller;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import de.hs_rm.de.milefiz.game.model.Board;
 import de.hs_rm.de.milefiz.game.model.BoardDTO;
+import de.hs_rm.de.milefiz.game.model.BoardMapper;
 import de.hs_rm.de.milefiz.game.service.GameService;
 
 @RestController
 @RequestMapping("/api/game")
 public class GameController {
 
-    private GameService gameService;
+    private final GameService gameService;
 
     public GameController(GameService gameService){
         this.gameService = gameService;
     }
 
     @GetMapping(path = "/getBoard")
-    public BoardDTO getBoard() throws StreamReadException, DatabindException, IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(new File("src/main/resources/static/boards/dummyBoard.json"), BoardDTO.class);
+    public BoardDTO getBoard() {
+        Board board = gameService.getTestBoard();
+        return BoardMapper.mapToDTO(board);
+
     }
 }
--- src/main/java/de/hs_rm/de/milefiz/game/service/GameServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/GameServiceImpl.java
@@ -38,8 +38,6 @@ public class GameServiceImpl implements GameService {
 
     public GameServiceImpl(DiceServiceImpl diceService, ApplicationEventPublisher publisher, CooldownServiceImpl cooldownService) throws IOException {
         final String BOARD_PATH = "static/boards/dummyBoard.json";
-
-        
         ObjectMapper objectMapper = new ObjectMapper();
 
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(BOARD_PATH);
@@ -49,9 +47,9 @@ public class GameServiceImpl implements GameService {
         }
 
         try{
-        BoardDTO testBoardDTO = objectMapper.readValue(inputStream, BoardDTO.class);
-        testBoard = BoardMapper.mapToBoard(testBoardDTO);
-        logger.info("Test board loaded successfully from: {}", BOARD_PATH);
+            BoardDTO testBoardDTO = objectMapper.readValue(inputStream, BoardDTO.class);
+            testBoard = BoardMapper.mapToBoard(testBoardDTO);
+            logger.info("Test board loaded successfully from: {}", BOARD_PATH);
         }finally{
             inputStream.close();
         }
