eaaf83bb9203d36c757010d0a0df26c81c45ff8c
Maximilian Ressel <Maximilian.Ressel@student.hs-rm.de>
2025-12-06 18:39:44 +0100

    fixed board datei

--- src/main/java/de/hs_rm/de/milefiz/game/service/GameServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/GameServiceImpl.java
@@ -58,7 +58,7 @@ public class GameServiceImpl implements GameService {
     public GameServiceImpl(DiceServiceImpl diceService, ApplicationEventPublisher publisher,
             CooldownServiceImpl cooldownService, MovementService movementService)
             throws IOException {
-        final String BOARD_PATH = "static/boards/dummyBoard.json";
+        final String BOARD_PATH = "boards/dummyBoard.json";
         ObjectMapper objectMapper = new ObjectMapper();
 
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(BOARD_PATH);
