d28f2f367c71e7ed1d1b021f08c00d9ccb1672fc
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-07 23:32:35 +0100

    Added Path to get from Game Start to Game pages

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GameView from '../views/GameView.vue'
+import GameStartView from '@/views/GameStartView.vue'
+
+const routes = [
+  {
+    path: '/',               // The initial route
+    name: 'game-start',
+    component: GameStartView,
+  },
+  {
+    path: '/game',           // The actual game page
+    name: 'game',
+    component: GameView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'game',
-      component: GameView,
-    },
-  ],
+  routes,
 })
 
 export default router
