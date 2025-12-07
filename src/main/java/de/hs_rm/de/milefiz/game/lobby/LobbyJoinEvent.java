51ec224dc737ebd8ff2165a9c5997a121fe8886f
Robin Claassen <Robin.Claassen@student.hs-rm.de>
2025-12-07 14:08:54 +0100

    fixed max´s javadoc fix

--- src/main/java/de/hs_rm/de/milefiz/game/lobby/LobbyJoinEvent.java
+++ src/main/java/de/hs_rm/de/milefiz/game/lobby/LobbyJoinEvent.java
@@ -4,6 +4,12 @@ import java.util.UUID;
 
 import de.hs_rm.de.milefiz.game.model.dto.LobbyDTO;
 
+/**
+ * @param playerId eigene playerId
+ * @param playerToken eigener (geheimer) PlayerToken
+ * @param lobby LobbyDTO mit allen Spielern und Daten
+ * @param msg optionale Nachricht für INformationen
+ */
 public record LobbyJoinEvent(UUID playerId, String playerToken, LobbyDTO lobby, String msg) {
 
 }
