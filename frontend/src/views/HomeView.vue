a992ce5ea74ea934ff067c3d676b58cd85b16ca0
Leon Schäfer <leon.schaefer@student.hs-rm.de>
2025-12-07 23:38:12 +0100

    #129 Background hinzugefügt, anpassung ans Wireframe

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -21,11 +21,37 @@ const router = useRouter()
 
 <style scoped>
 .home {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  margin-top: 7rem;
+
   align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  padding-bottom: 8rem;
+}
+
+.home::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("/backgrounds/BackgroundTest.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  filter: blur(4px);
+  z-index: -1; 
+}
+
+.header-with-plate,
+.button-container {
+  position: relative;
+  z-index: 1;
 }
 
 .header-with-plate {
@@ -36,8 +62,8 @@ const router = useRouter()
 .button-container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 1rem;
   align-items: center;
-  margin-top: -6rem;  /* Verkleinert Abstand zum Header */
+  margin-top: -3.5rem;  /* Verkleinert Abstand zum Header */
 }
 </style>
