84b33592047b10d2393fe44d689760b78001af84
Robert Seeger <robert.seeger@student.hs-rm.de>
2025-12-10 22:06:50 +0100

    LobbyList erstellt

---
+++ frontend/src/components/ui/pages/LobbyList.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+
+/* TODO: vielleicht lobby interface aus lobbyupdate (vielleicht anpassen?) */
+export interface Lobby {
+    id: string,
+    name: string,
+    //...
+}
+
+defineProps<{ lobbies : Lobby[]}>()
+const lobbyid = defineModel('lobbyid')
+
+// für filtern https://vuejs.org/guide/essentials/list.html#displaying-filtered-sorted-results
+</script>
+
+<template>
+    <div class="lobby-list-container">
+        <label>Lobbys</label>
+        <div class="lobby-list">
+            <select v-model="lobbyid" multiple>
+                  <option :value="lobby.id" class="lobby-item" v-for="(lobby) in lobbies" :key="lobby.id" >
+                        {{ lobby.id }} / {{ lobby.name }}
+                  </option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .component-list {
+        display: flex;
+        height: 50%;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-items: center;
+        gap: 2vh;
+    }
+
+    .component-list > * {
+        flex: 1;
+    }
+</style>
--- frontend/src/views/LobbyListView.vue
+++ frontend/src/views/LobbyListView.vue
@@ -1,12 +1,27 @@
-<script setup>
+<script lang="ts" setup>
 import Header from '@/components/ui/pages/Header.vue'
 import ComponentList from '@/components/ui/pages/ComponentList.vue'
+import LobbyList, {type Lobby} from '@/components/ui/pages/LobbyList.vue'
+import { ref } from 'vue'
+
+const lobbies = ref<Lobby[]>([])
+const selectedLobby = ref<string>()
+
+  /* ## Testcode ## */
+const addtest = () => {
+  if (lobbies.value)
+    lobbies.value.push({id: Math.random().toString(36).slice(2), name: Math.random().toString(36).slice(2)})
+}
+  /* #### */
 </script>
 
 <template>
   <div class="lobbylist">
     <Header>Spiel Beitreten</Header>
     <ComponentList>
+      {{ selectedLobby }}
+      <button @click="addtest">click</button>
+      <LobbyList v-model:lobbyid="selectedLobby" :lobbies="lobbies" />
     </ComponentList>
   </div>
 </template>
