b1cfeea08511f4e1bf912856f60902b916e94b8b
ktran001 <kevin.tran@student.hs-rm.de>
2025-12-10 01:50:12 +0100

    EnergyBar mit hardcoded fortschritts value

---
+++ frontend/src/components/ui/EnergyBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+/* erstmal hardcoded energy */
+const maxEnergy = 6
+const currentEnergy = ref(2)
+
+const barWidth = computed(() => (currentEnergy.value / maxEnergy) * 100 + "%")
+</script>
+
+<template>
+    <div class="energy-bar">
+        <div class="saved-energy" :style="{ width: barWidth }"></div>
+    </div>
+</template>
+
+<style>
+.energy-bar {
+    position: absolute;
+    width: 25vw;
+    height: 30px;
+    
+    background-color: #666;
+    border: 4px solid #004b2e;
+    border-radius: 10px;
+    box-sizing: border-box;
+}
+
+.saved-energy {
+    height: 100%;
+    background-color: rgb(1, 208, 1);
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;;
+    transition: width 0.3s ease;
+}
+</style>
--- frontend/src/components/ui/GameHUD.vue
+++ frontend/src/components/ui/GameHUD.vue
@@ -4,23 +4,33 @@
 <script setup lang="ts">
 import DiceButton from './DiceButton.vue'
 import DiceCounter from './DiceCounter.vue';
+import EnergyBar from './EnergyBar.vue';
 </script>
 
 
 <template>
   <div class="hud-container">
+    <!-- Würfelergebnis -->
     <div class="dice-counter-container">
       <DiceCounter />
     </div>
-    <div style="position: absolute;bottom: 40px;right: 0px;">
+
+    <!-- Button Bar -->
+    <div style="position: absolute;bottom: 2vw; right: 0px;">
       <div class="button-bar">
         <DiceButton />
       </div>
     </div>
+
+    <div style="position: absolute;bottom: 5vw; left: 3vw;">
+      <div class="energy-bar-container">
+        <EnergyBar />
+      </div>
+    </div>
+
   </div>
 </template>
 <style>
-
 .hud-container {
   position: fixed;
   inset: 0;
@@ -30,9 +40,12 @@ import DiceCounter from './DiceCounter.vue';
 
 .dice-counter-container {
   position: absolute;
-  top: 20px;           /* Abstand von oben */
-  left: 50%;           /* Mittig horizontal */
-  transform: translateX(-50%);  /* Zentriert das Element */
+  top: 20px;
+  /* Abstand von oben */
+  left: 50%;
+  /* Mittig horizontal */
+  transform: translateX(-50%);
+  /* Zentriert das Element */
 }
 
 .button-bar {
@@ -48,5 +61,17 @@ import DiceCounter from './DiceCounter.vue';
   width: 100%;
   box-sizing: border-box;
   right: 0px;
+}
+
+.energy-bar-container {
+  display: flex;
+  gap: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  padding-right: 25px;
+  border-radius: 8px;
+  width: 25vw;
+  box-sizing: border-box;
 }
 </style>
