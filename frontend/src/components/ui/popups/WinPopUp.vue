c6dd3d400553054dce292515ea278532ff27be48
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-10 17:05:01 +0100

    Buttons für PopUp Navigation hinzugefügt

--- frontend/src/components/ui/popups/WinPopUp.vue
+++ frontend/src/components/ui/popups/WinPopUp.vue
@@ -1,17 +1,28 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router'
 import BackButton from '../pages/BackButton.vue';
 
+const router = useRouter()
+
+const newGameStart = () => {
+  router.push({ name: 'game-start' })
+}
 
 </script>
 
 <template>
 	<div class="popup">
-		<div class="popup-inner">
-            <BackButton :to="{ name: 'Homepage' }" class="popup-close"/>
+		
+
+
+		<div class="button-container">
+			<BackButton :to="{ name: 'Homepage' }">&lt; Hauptmenü</BackButton>
+			
+      		<button class="menu-button" @click="newGameStart">Neues Spiel</button>
 		</div>
 	</div>
+
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
