8040848110da0a509749951bbad454c13b26f6a4
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-11 22:52:15 +0100

    Added css for buttons and popup area

--- frontend/src/components/ui/popups/WinPopUp.vue
+++ frontend/src/components/ui/popups/WinPopUp.vue
@@ -6,23 +6,49 @@ import BackButton from '../pages/BackButton.vue';
 const router = useRouter()
 
 const newGameStart = () => {
-  router.push({ name: 'game-start' })
+	router.push({ name: 'game-start' })
 }
 
 </script>
 
 <template>
 	<div class="popup">
-		
+		<span>Spieler 1 hat gewonnen!</span>
 
 
 		<div class="button-container">
 			<BackButton :to="{ name: 'Homepage' }">&lt; Hauptmenü</BackButton>
-			
-      		<button class="menu-button" @click="newGameStart">Neues Spiel</button>
+
+			<button class="menu-button" @click="newGameStart">Neues Spiel</button>
 		</div>
 	</div>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.popup {
+  position: relative;
+  height: 50vh;
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  overflow: hidden;
+  padding-bottom: 4rem;
+
+  z-index: 100;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  gap: 2vh;
+  align-items: center;
+}
+
+.menu-button:hover {
+  transform: scale(1.05);
+  transition: transform 0.2s ease;
+}
+</style>
