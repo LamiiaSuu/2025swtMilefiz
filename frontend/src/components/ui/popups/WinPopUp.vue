1e4082065668a3c42c80207d3c9523d26fe7735f
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-12 15:12:50 +0100

    CSS for proper centering and some other formatting of the popup, currently in MapEditor for testing

--- frontend/src/components/ui/popups/WinPopUp.vue
+++ frontend/src/components/ui/popups/WinPopUp.vue
@@ -1,54 +1,84 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import BackButton from '../pages/BackButton.vue';
+import BackButton from '../pages/BackButton.vue'
 
 const router = useRouter()
 
 const newGameStart = () => {
-	router.push({ name: 'game-start' })
+  router.push({ name: 'game-start' })
 }
-
 </script>
 
 <template>
-	<div class="popup">
-		<span>Spieler 1 hat gewonnen!</span>
+  <div class="page">
+    <div class="popup">
 
 
-		<div class="button-container">
-			<BackButton :to="{ name: 'Homepage' }">&lt; Hauptmenü</BackButton>
+      <div class="win-message">
+        <h1>Spieler 1 hat gewonnen!</h1>
+      </div>
 
-			<button class="menu-button" @click="newGameStart">Neues Spiel</button>
-		</div>
-	</div>
 
+
+      <div class="button-container">
+        <BackButton :to="{ name: 'Homepage' }">&lt; Hauptmenü</BackButton>
+        <button class="menu-button" @click="newGameStart">Neues Spiel</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.page {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+}
+
 .popup {
   position: relative;
-  height: 50vh;
-  width: 50vw;
   display: flex;
   flex-direction: column;
-
   align-items: center;
-  overflow: hidden;
-  padding-bottom: 4rem;
 
+  margin: auto;
+  width: 75vw;
+  height: 75vh;
+
+  padding: 1vh;
+
+  overflow: hidden;
+
+  background-color: var(--button-color);
   z-index: 100;
+  border: 5px solid red;
 }
 
 .button-container {
   display: flex;
   flex-direction: row;
-  gap: 2vh;
+  gap: 4vh;
   align-items: center;
+  width: 100%;
+
+  border: 5px solid yellow;
+}
+
+.button-container :deep(button) {
+  flex: 1;
 }
 
 .menu-button:hover {
   transform: scale(1.05);
   transition: transform 0.2s ease;
+}
+
+h1 {
+
+  font-weight: bolder;
+  color: #0A0A0A;
+  paint-order: stroke fill;
 }
 </style>
--- frontend/src/views/MapEditorView.vue
+++ frontend/src/views/MapEditorView.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+    import WinPopUp from '@/components/ui/popups/WinPopUp.vue';
+</script>
+
 <template>
     <p>Map Editor</p>
+
+    <WinPopUp />
 </template>
