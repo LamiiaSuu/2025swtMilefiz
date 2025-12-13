36f379b203e9f5f3750125441e982d23cc827a03
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-12-13 17:34:27 +0100

    #144 Gewinndialog milefiz gameFinished - gameFinished wird im Store geupdated

--- frontend/src/stores/milefizstore.ts
+++ frontend/src/stores/milefizstore.ts
@@ -174,7 +174,7 @@ export const useMilefizStore = defineStore('milefizstore', () => {
         if (event.type === "WIN"){
           boardStore.updateMeeplePosition(event.meepleId, event.targetField)
           gamedata.currentDiceRoll = 0
-          //TODO meeple bei spieler und von board entfernen
+          gameFinished.value = true
         }
          if (event.type === "BARRIER_MOVE_ERROR"){
           console.warn("Barriermove rejected:", event.msg)
