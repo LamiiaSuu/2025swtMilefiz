b8886eb8214d5eaa627bfb099f0eb002f56ad1a8
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 13:01:13 +0100

    Milefizstore Pinia Variablen hinzugefügt - gameFinished ref - winnerName ref

--- frontend/src/stores/milefizstore.ts
+++ frontend/src/stores/milefizstore.ts
@@ -42,6 +42,9 @@ export const useMilefizStore = defineStore('milefizstore', () => {
     isEnergyFresh: false,
   })
 
+  const gameFinished = ref(false)
+  const winnerName = ref<string | null>(null)
+
   // Beispiele für Daten
   const gamedata = reactive<{
     playerId: string
