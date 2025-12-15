1017b65400c2adbf4144720e3073d4750ec2b07e
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 14:02:09 +0100

    Win Event gibt jetzt PlayerColor mit

--- src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
@@ -218,7 +218,7 @@ public class MovementServiceImpl implements MovementService {
             if (player.getRemainingMoves() == LAST_MOVE) {
                 player.useMove();
                 logger.info("player {} has won", player.getPlayerName());
-                return new FrontendPlayerHasWonEvent(player.getPlayerName(), meeple.getId(), nextField.getId());
+                return new FrontendPlayerHasWonEvent(player.getPlayerName(), player.getColor(), meeple.getId(), nextField.getId());
             }
             logger.info("Cant enter End with remaining moves");
             return new FrontendMoveRejectedEvent("Cant enter End with remaining Moves");
