11e3e86e6115fbfa76485ff0f63c99c30128d1c7
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 12:51:34 +0100

    Added BackButton to PopUp

--- frontend/src/components/ui/popups/WinPopUp.vue
+++ frontend/src/components/ui/popups/WinPopUp.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import BackButton from '../pages/BackButton.vue';
+
 
 </script>
 
 <template>
 
+	<div class="popup">
+		<div class="popup-inner">
+            <BackButton :to="{ name: 'Homepage' }" class="popup-close"/>
+		</div>
+	</div>
 </template>
 
 <style scoped>
