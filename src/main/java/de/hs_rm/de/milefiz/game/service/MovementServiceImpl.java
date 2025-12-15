607ccc05eb9ba84ea81750c9d308694f2a72df5d
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 13:21:16 +0100

    BAckend Syntax angepasst

--- src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
@@ -217,8 +217,8 @@ public class MovementServiceImpl implements MovementService {
             // Wenn man darauf endet, wird der meeple entfernt.
             if (player.getRemainingMoves() == LAST_MOVE) {
                 player.useMove();
-                logger.info("player {} has won", player.getName());
-                return new FrontendPlayerHasWonEvent(player.getName(), meeple.getId(), nextField.getId());
+                logger.info("player {} has won", player.getPlayerName());
+                return new FrontendPlayerHasWonEvent(player.getPlayerName(), meeple.getId(), nextField.getId());
             }
             logger.info("Cant enter End with remaining moves");
             return new FrontendMoveRejectedEvent("Cant enter End with remaining Moves");
