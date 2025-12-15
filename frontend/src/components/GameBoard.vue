e75c48b28a8b81f76dba378f0301f09a7f465f62
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 13:16:20 +0100

    WinPopUp in GameBoard.vue eingebunden + Fade

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -361,6 +361,10 @@ const meepleColorMap = computed(() => {
 </script>
 
 <template>
+  <transition name="fade">
+    <WinPopup v-if="milefizStore.gameFinished" />
+  </transition>
+
   <!-- 3D-Canvas Element das den ganzen Bildschirm ausfüllt-->
   <TresCanvas window-size style="width: 100vw; height: 100vh" clear-color="#87CEEB">
     <!-- Kameraposition und Kamerasteuerung via OrbitControls -->
@@ -427,5 +431,14 @@ const meepleColorMap = computed(() => {
   /* <-- immer weiß */
   box-shadow: 0 0 6px rgba(0, 0, 0, 0.5);
   transition: background 0.1s ease, transform 0.1s ease;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
