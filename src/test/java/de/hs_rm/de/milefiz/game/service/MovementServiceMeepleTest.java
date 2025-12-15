4cbfae4374e956c97a539ea88447c516a91fbc20
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2025-12-15 13:20:02 +0100

    Backend Test Syntax angepasst

--- src/test/java/de/hs_rm/de/milefiz/game/service/MovementServiceMeepleTest.java
+++ src/test/java/de/hs_rm/de/milefiz/game/service/MovementServiceMeepleTest.java
@@ -206,7 +206,7 @@ public class MovementServiceMeepleTest {
         assertInstanceOf(FrontendPlayerHasWonEvent.class, result);
 
         FrontendPlayerHasWonEvent evt = (FrontendPlayerHasWonEvent) result;
-        assertEquals(player.getName(), evt.playerName());
+        assertEquals(player.getPlayerName(), evt.playerName());
 
         assertEquals(0, player.getRemainingMoves());
     }
