e339bdef82bf3a70d39e6f8ff95238071be583f1
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-12-18 23:44:49 +0100

    MapEditorView v0.1.0 BasisVue aufgesetzt - Map-Editor-Vue hat schon den Button um zurück ins hauptmenü zu gehen und den Hintergrund des Hauptmenüs nur mehr blurred

--- frontend/src/views/MapEditorView.vue
+++ frontend/src/views/MapEditorView.vue
@@ -1,3 +1,86 @@
+<script lang="ts" setup>
+import BackButton from '@/components/ui/pages/BackButton.vue'
+
+
+</script>
+
 <template>
-    <p>Map Editor</p>
+    <div class="mapeditor">
+        <div class="form-row">
+            <div class="button-container">
+                <BackButton :to="{ name: 'Homepage' }" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.mapeditor {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  overflow: hidden;
+  padding-bottom: 4rem;
+}
+
+.mapeditor::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("/backgrounds/BackgroundTest.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  filter: blur(4px);
+  z-index: -1;
+}
+
+form {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 40px;
+    width: 100%;
+    justify-content: center;
+    margin-top: -5vh;
+}
+
+.form-column {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.form-row {
+    display: grid;
+    grid-template-columns: 150px 1fr;
+    align-items: center;
+    gap: 15px;
+    z-index: 20;
+}
+
+.form-row label {
+    text-align: right;
+}
+
+.button-container {
+  position: absolute;
+  bottom: 20px;
+  left: 30px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2vh;
+}
+
+.menu-button:hover {
+  transform: scale(1.05);
+  transition: transform 0.2s ease;
+}
+</style>
