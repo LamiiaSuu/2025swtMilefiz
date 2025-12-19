892556ea207cb7de9f9165ca70958c3f5846f2e8
Robin Claassen <Robin.Claassen@student.hs-rm.de>
2025-12-19 16:44:04 +0100

    Config für SPA Index-page

--- src/main/java/de/hs_rm/de/milefiz/configuration/SpaRedirectConfig.java
+++ src/main/java/de/hs_rm/de/milefiz/configuration/SpaRedirectConfig.java
@@ -1,8 +1,13 @@
 package de.hs_rm.de.milefiz.configuration;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class SpaRedirectConfig implements WebMvcConfigurer {
@@ -12,11 +17,32 @@ public class SpaRedirectConfig implements WebMvcConfigurer {
      * Seiten geforwardet, sodass man clientseitig mehr Kontrolle hat. z.B. für
      * join-Endpunkt
      */
+    // @Override
+    // public void addViewControllers(ViewControllerRegistry registry) {
+    //     registry.addViewController("/{path:^(?!api|actuator|ws|assets|static|favicon\\.ico).*$}")
+    //             .setViewName("forward:/index.html");
+    //     registry.addViewController("/**")
+    //             .setViewName("forward:/index.html");
+    // }
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{path:^(?!api|actuator|ws|assets|static|favicon\\.ico).*$}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**")
-                .setViewName("forward:/index.html");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        // Exclude backend endpoints
+                        if (resourcePath.startsWith("api/")
+                                || resourcePath.startsWith("ws")
+                                || resourcePath.startsWith("sockjs")) {
+                            return null;
+                        }
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return (requestedResource.exists() && requestedResource.isReadable())
+                                ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
