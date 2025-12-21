77a5f1c48e66f73f2f6a2d9116fcc064eddd1ca3
Robothfe <robert.bothfeld@student.hs-rm.de>
2025-12-21 13:40:48 +0100

    #268_Einstellungen_Vue SettingsVue - SettingsVue initialisiert und vom hauptmenü abgeleitet

--- frontend/src/views/SettingView.vue
+++ frontend/src/views/SettingView.vue
@@ -1,3 +1,58 @@
+
+
+<script setup>
+import BackButton from '@/components/ui/pages/BackButton.vue'
+import Header from '@/components/ui/pages/Header.vue'
+
+</script>
+
 <template>
-    <p>Settings</p>
+  <div class="settings">
+    <Header overlay>Einstellung</Header>
+        <div class="settings-button-container">
+            <BackButton :to="{ name: 'Homepage' }" />
+        </div>
+  </div>
 </template>
+
+<style scoped>
+.settings {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  overflow: hidden;
+  padding-bottom: 4rem;
+}
+
+.settings::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("/backgrounds/BackgroundTest.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  filter: blur(4px);
+  z-index: -1;
+}
+
+.settings-button-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2vh;
+  align-items: center;
+}
+
+.settings-menu-button:hover {
+  transform: scale(1.05);
+  transition: transform 0.2s ease;
+}
+
+</style>
