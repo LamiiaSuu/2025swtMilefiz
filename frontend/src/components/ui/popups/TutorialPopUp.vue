8c8d64a187251f5444337e8b66db635f14eaf9a5
Jaqueline Huth <jaqueline.huth@student.hs-rm.de>
2026-01-05 23:22:45 +0100

    #308 - Creted .vue files

---
+++ frontend/src/components/ui/popups/TutorialPopUp.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import Header from '@/components/ui/pages/Header.vue'
+import { useAudioStore } from '@/stores/audioStore'
+import { tUI } from '@/i18n'
+import LanguageSelection from '@/components/ui/LanguageSelection.vue'
+
+const router = useRouter()
+const audio = useAudioStore()
+
+
+</script>
+
+<template>
+  <div class="home">
+    <!--MI'lefiz Header -->
+    <Header overlay></Header>
+
+    <!-- Language Selection-->
+    <LanguageSelection></LanguageSelection>
+
+  </div>
+</template>
+
+<style scoped>
+.home {
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  overflow: hidden;
+  padding-bottom: 4rem;
+}
+
+.home::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("/backgrounds/BackgroundTest.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  filter: blur(4px);
+  z-index: -1;
+}
+
+
+</style>
