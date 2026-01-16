ed28c1ca345fc5fdd2ed0041e9a10ebcd9bd4b72
Maximilian Ressel <Maximilian.Ressel@student.hs-rm.de>
2026-01-16 01:55:33 +0100

    vorerst abandoned

--- src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
+++ src/main/java/de/hs_rm/de/milefiz/game/service/MovementServiceImpl.java
@@ -159,6 +159,18 @@ public class MovementServiceImpl implements MovementService {
             return new FrontendCheatedEvent(player.getId(), "Attempt to switch Meeple during move failed.");
         }
 
+        //Meeple ist stuck, Zug wird zurückgesetzt, sodass der Spieler der Meeple wechseln kann
+        if (!existsLegalStopWithinRemainingMoves(currentField, lastField, player.getRemainingMoves(),
+                otherOwnMeepleFields, barrierFields, rivalMeeples, rivalMeepleFields)) {
+            endTurnWithMove(player, meeple, currentField);
+            return new FrontendMoveWithLossEvent(
+                    player.getId(),
+                    meeple.getId(),
+                    currentField.getId(),
+                    player.getRemainingMoves(),
+                    player.hasMoved());
+        }
+
         // Ziel-Feld anhand der Bewegungsrichtung bestimmen
         Field nextField = getNextFieldByDirection(currentField, direction);
 
