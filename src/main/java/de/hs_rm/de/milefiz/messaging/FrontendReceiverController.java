83a67556cbc51c133e3b6011a04887ba80eae3e5
Robin Claassen <robin.claassen@student.hs-rm.de>
2026-01-16 22:43:00 +0100

    Fixing javadoc

--- src/main/java/de/hs_rm/de/milefiz/messaging/FrontendReceiverController.java
+++ src/main/java/de/hs_rm/de/milefiz/messaging/FrontendReceiverController.java
@@ -203,9 +203,6 @@ public class FrontendReceiverController {
      *
      * @param lobbyId die eindeutige UUID der Lobby in der gewürfelt wird
      * @param command der Würfel-Befehl vom Client, enthält die Spieler-ID
-     * @param requestedValue angefragte Würfelzahl für Demo zwecke, überschreibt
-     * nicht die Regelung von z.B. cooldown | {@link enableRequestedDiceRolls}
-     * muss auf true sein
      * @return {@link FrontendRollDiceEvent} mit Lobby-ID und Würfelergebnis
      * (1-6)
      *
