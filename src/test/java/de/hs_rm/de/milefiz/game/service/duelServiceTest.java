ebc7b5f8a03672bd45824f01ff5b0b9d6dcf8643
Robothfe <robert.bothfeld@student.hs-rm.de>
2026-01-17 12:46:50 +0100

    Unit Test Fix - Minigames wurden nicht entfernt nach den Games. Das war ein legitimier merge fehler - Mock verändert für MovementServiceTest um an den Code anzupassen, gab vorher ne NPE

--- src/test/java/de/hs_rm/de/milefiz/game/service/duelServiceTest.java
+++ src/test/java/de/hs_rm/de/milefiz/game/service/duelServiceTest.java
@@ -126,7 +126,7 @@ class DuelServiceImplTest {
         var games = service.getGames();
 
         assertNotNull(games);
-        assertEquals(6, games.size());
+        assertEquals(7, games.size());
     }
 
     @Test
