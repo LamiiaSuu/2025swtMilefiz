74d2636ffab6de217fe4114cfc1f7ff461038e70
Robothfe <robert.bothfeld@student.hs-rm.de>
2026-01-20 14:44:42 +0100

    ColorBrainGame fix - If-Abfrage Spieler in Klammern gesetzt

--- frontend/src/stores/milefizstore.ts
+++ frontend/src/stores/milefizstore.ts
@@ -484,7 +484,7 @@ export const useMilefizStore = defineStore('milefizstore', () => {
         }
 
         if (event.type === "COLORBRAIN_GAME_UPDATE") {
-          if (!activeDuels[event.duelId] && event.player1 === gamedata.playerId || event.player2 === gamedata.playerId) {
+          if (!activeDuels[event.duelId] && (event.player1 === gamedata.playerId || event.player2 === gamedata.playerId)) {
             activeDuels[event.duelId] = { duelId: event.duelId, state: {} }
           }
           const duel = activeDuels[event.duelId]
